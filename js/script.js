577f263fb788a0e2604124f039ba6bd0625b0ca9
Mativve <mativve@gmail.com>
2018-04-18 16:46:08 +0200

    0.2
    New version

--- js/script.js
+++ js/script.js
@@ -1,147 +1,206 @@
-//Variables
+$(document).ready(function () {
 
-///Red stats
-var r = 50;
-var rclick = 0;
+    var slowmo = new Audio("http://freesound.org/data/previews/123/123437_1149625-lq.mp3");
 
-///Blue stats
-var b = 50;
-var bclick = 0;
+    var red = 50,
+        win_red = 0,
+        click_red = 0,
+        key_red = 75,
 
-///Game state
-var g = 0;
-var cd = 3;
+        blue = 50,
+        win_blue = 0,
+        click_blue = 0,
+        key_blue = 83,
 
+        play = false,
+        count = 3,
+        round = 0;
 
-//Audio effect
-///Countdown sound
-var cd_audio = new Audio('audio/321f.wav');
+    var $info = $(".info");
 
-///Count down 
-var cda = 0;
+    //Settings
+    var shakeAnimate = true,
+        light = false;
 
-//Finish him sound
-var fh = new Audio('audio/finishhim.wav');
-
-//Are you ready to rumble sound
-var rumble = new Audio('audio/rumble.wav');
-
-
-//Start game on load js
-countdown();
-
-function countdown() {
-    cd_audio.play();
-
-    if (cd > 0) {
-        $("#togame").text(cd);
-        cd--;
-        setTimeout(countdown, 1000);
-        g = 0;
-    } else {
-        $("#togame").text("FIGHT!");
-        g = 1;
+    function start() {
+        play = true;
     }
-}
 
-//Detect click red/blue player
-document.addEventListener("keyup", keyPush);
-function keyPush(evt) {
-    if (g == 1) {
-        switch (evt.keyCode) {
-            case 75:
-                r++;
-                b--;
-                changewidth(r, b);
-                rclick++;
-                $("#stats .right h2").text(rclick);
+    function reset() {
+        red = 50;
+        click_red = 0;
 
-                //console.log(r + ", " + b);
-                break;
-            case 83:
-                r--;
-                b++;
-                bclick++;
-                $("#stats .left h2").text(bclick);
+        blue = 50;
+        click_blue = 0;
 
-                changewidth(r, b);
-                //console.log(r + ", " + b);
-                break;
+        count = 3;
+        barWidth();
+        console.log("Reset done!");        
+    }
+
+    function countDown() {
+        reset();
+        console.log("Start countdown");
+
+        round++;
+
+        let click = new Audio("http://freesound.org/data/previews/384/384187_6712901-lq.mp3");
+        click.play();
+
+        $info.html("<p>" + count + "</p>");
+
+        let timer = setInterval(function () {
+            let click = new Audio("http://freesound.org/data/previews/384/384187_6712901-lq.mp3");
+            click.play();
+
+            if (count == 1) {
+                clearInterval(timer);
+                start();
+                $info.removeClass("show");
+                console.log(play);
+            } else {
+                count--;
+                $info.html("<p>" + count + "</p>");
+            }
+        }, 1000);
+
+        $info.addClass("show");
+    }
+    //    countDown();
+
+    function checkWin() {
+        if (red >= 100) {
+            console.log("Red Win!");
+            var win = new Audio("http://freesound.org/data/previews/269/269184_4409114-lq.mp3");
+            win.play();
+            play = false;
+            win_red++;
+            updateStats();
+
+            $info.addClass("show");
+            $info.html("<p>Red win!</p><br><p><button class='btn startGame'>Try again&nbsp;<i class='fas fa-play'></i></button></p>");
+            $(".startGame").click(countDown);
+        } else if (blue >= 100) {
+            console.log("Blue Win!");
+            var win = new Audio("http://freesound.org/data/previews/269/269184_4409114-lq.mp3");
+            win.play();
+            play = false;
+            win_blue++;
+            updateStats();
+
+            $info.addClass("show");
+            $info.html("<p>Blue win!</p><br><p><button class='btn startGame'>Try again&nbsp;<i class='fas fa-play'></i></button></p>");
+            $(".startGame").click(countDown);
         }
     }
-}
 
+    function barWidth() {
 
-//Change width bars
-function changewidth(red, blue) {
+        $(".bar.red").css({
+            "width": red + "%"
+        });
+        $(".bar.blue").css({
+            "width": blue + "%"
+        });
 
-    //Finish him checker
-    if ((red > 90 && red < 110) || (blue > 90 && blue < 110)) {
-        $("#info").text("Finish him!");
-    } else {
-        $("#info").text("");
-    }
+        if (red > 90 && red < 100) {
+            if (shakeAnimate) {
+                $(".battle").addClass("shakeRed");
+            } else {
+                $(".battle").css({
+                    transform: "translate(20%, -50%) scale(2)"
+                });
+            }
+        } else if (blue > 90 && blue < 100) {
+            if (shakeAnimate) {
+                $(".battle").addClass("shakeBlue");
+            } else {
+                $(".battle").css({
+                    transform: "translate(-120%, -50%) scale(2)"
+                });
+            }
 
-    //Check who win
-    if (red > 100 && g == 1) {
-        $("#info").text("Red win!");
-        g = 0;
-        $("#game").css("color", "#c73f3f");
-    } else if (blue > 100 && g == 1) {
-        $("#info").text("Blue win!");
-        g = 0;
-        $("#game").css("color", "#3f60c7");
-
-    } else {
-
-        //Change glow color
-        if (red > blue) {
-            $(".bar").removeClass("blueshadow");
-            $(".bar").addClass("redshadow");
-
-        } else if (blue > red) {
-            $(".bar").removeClass("redshadow");
-            $(".bar").addClass("blueshadow");
-        } else {
-            $(".bar").removeClass("blueshadow");
-            $(".bar").removeClass("redshadow");
+        } else if (blue <= 90 || red <= 90) {
+            if (shakeAnimate) {
+                $(".battle").removeClass("shakeRed").removeClass("shakeBlue");
+            } else {
+                $(".battle").css({
+                    transform: "translate(-50%, -50%) scale(1)"
+                });
+            }
         }
 
-        //Change width bar
-        $("#blue").css("width", blue + "%");
-        $("#red").css("width", red + "%");
-
-
+        checkWin();
     }
-}
+
+    var $blueStat = $(".top .left");
+    var $redStat = $(".top .right");
+    var $roundStat = $(".top .center");
+
+    function updateStats() {
+        $blueStat.html("<p>" + click_blue + "</p>");
+        $redStat.html("<p>" + click_red + "</p>");
+
+        let html = "<div class='center'><p>Round " + round + "</p><p>" + win_blue + ":" + win_red + "</p></div>";
+
+        $roundStat.html(html);
+    }
 
 
-//Reset the game
-function reset() {
-    r = 50;
-    b = 50;
-    g = 0;
-    rclick = 0;
-    bclick = 0;
-    cd = 3;
-    setTimeout(countdown, 2000);
-    cda = 0;
-    $("#info").text("");
-    $("#blue").css("width", b + "%");
-    $("#red").css("width", r + "%");
-    $(".bar").removeClass("blueshadow");
-    $(".bar").removeClass("redshadow");
-    $("#game").css("color", "#fff");
-    $("#stats .left h2").text(bclick);
-    $("#stats .right h2").text(rclick);
-    $("#togame").text("Are you ready to rumble?");
-    rumble.play();
-}
+    document.addEventListener("keyup", keyPush);
+
+    function keyPush(e) {
+        if (play == false) return;
+        console.log(String.fromCharCode(e.keyCode));
+        if (e.keyCode == key_red) {
+            if (red < 100) {
+                red++;
+                click_red++;
+
+                blue--;
+
+                console.log("Red click");
+            }
+        }
+
+        if (e.keyCode == key_blue) {
+            if (blue < 100) {
+                blue++;
+                click_blue++;
+
+                red--;
+
+                console.log("Blue click");
+            }
+        }
+        barWidth();
+        updateStats();
+    }
 
 
-//
-//setting panel
-//
+    $(".settingbtn").click(function () {
+        play = !play;
+        $(".settings").toggleClass("show");
+    });
 
-$("#settingbtn").click(function(){ $("#setting_panel").show(); });
-$("#exitbtn").click(function(){ $("#setting_panel").hide(); });
+    $("#animation").on("input change", function () {
+        shakeAnimate = !shakeAnimate;
+        if (($(".battle").hasClass("shakeRed")) || ($(".battle").hasClass("shakeBlue"))) {
+            $(".battle").removeClass("shakeRed").removeClass("shakeBlue");
+        }
+        console.log("Zmieniono animację na " + shakeAnimate);
+    });
+
+    $(".startGame").click(countDown);
+    
+    $("#blueKey").on("input change",function(){
+        key_blue = parseFloat($(this).val());
+        $(".bar.blue").html(String.fromCharCode(key_blue));
+    });
+    
+    $("#redKey").on("input change",function(){
+        key_red = $(this).val();
+        $(".bar.red").html(String.fromCharCode(key_red));
+    });    
+
+});
